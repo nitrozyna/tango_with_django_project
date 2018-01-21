ceb1ae13dc60c35122cd4e7b4bd2fed64b687212
nitrozyna <kurkiewicz.margaret@gmail.com>
2018-01-21 20:27:09 +0000

    Chapter 3 completed with additional exercises.

---
+++ rango/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+
+def about(request):
+	return HttpResponse("""Rango says here is the about page.<a href="/rango/">Index</a>""")
